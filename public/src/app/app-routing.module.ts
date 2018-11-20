24c27b9cf153ddd95b3b707b1d100b94d3c614a2
Jared Avila <jared.c.avila@gmail.com>
2018-11-20 12:53:52 -0800

    pulling

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-<<<<<<< HEAD
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { RegistrationComponent } from "./registration/registration.component";
@@ -9,12 +8,14 @@ import { NewAuctionComponent } from "./new-auction/new-auction.component";
 import { ShowAuctionComponent } from "./show-auction/show-auction.component";
 import { SimilarItemsComponent } from "./similar-items/similar-items.component";
 import { AuctionMenuComponent } from "./auction-menu/auction-menu.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegistrationComponent },
+  { path: "user-profile/:id", component: UserProfileComponent },
   { path: "auctions/new", component: NewAuctionComponent },
   {
     path: "auctions/:id",
@@ -29,21 +30,6 @@ const routes: Routes = [
       { path: "menu", component: AuctionMenuComponent }
     ]
   }
-=======
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { NewAuctionComponent } from './new-auction/new-auction.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrationComponent },
-  { path: 'auctions/new', component: NewAuctionComponent },
-  { path: 'user-profile/:id', component: UserProfileComponent },
->>>>>>> 28ce6d520d22da33717d807f03b42593a072c60c
 ];
 
 @NgModule({
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,25 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { Ng2CarouselamosModule } from 'ng2-carouselamos';
-import { DurationPickerModule } from 'ngx-duration-picker'
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { Ng2CarouselamosModule } from "ng2-carouselamos";
+import { DurationPickerModule } from "ngx-duration-picker";
 
-import { AppRoutingModule } from './app-routing.module';
-import { HttpService } from './http.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpService } from "./http.service";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { NewAuctionComponent } from './new-auction/new-auction.component';
-<<<<<<< HEAD
-import { ShowAuctionComponent } from './show-auction/show-auction.component';
-import { SimilarItemsComponent } from './similar-items/similar-items.component';
-import { AuctionMenuComponent } from './auction-menu/auction-menu.component';
-=======
-import { UserProfileComponent } from './user-profile/user-profile.component';
->>>>>>> 28ce6d520d22da33717d807f03b42593a072c60c
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { RegistrationComponent } from "./registration/registration.component";
+import { NewAuctionComponent } from "./new-auction/new-auction.component";
+import { ShowAuctionComponent } from "./show-auction/show-auction.component";
+import { SimilarItemsComponent } from "./similar-items/similar-items.component";
+import { AuctionMenuComponent } from "./auction-menu/auction-menu.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 @NgModule({
   declarations: [
@@ -28,13 +25,10 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     LoginComponent,
     RegistrationComponent,
     NewAuctionComponent,
-<<<<<<< HEAD
     ShowAuctionComponent,
     SimilarItemsComponent,
-    AuctionMenuComponent
-=======
+    AuctionMenuComponent,
     UserProfileComponent
->>>>>>> 28ce6d520d22da33717d807f03b42593a072c60c
   ],
   imports: [
     BrowserModule,
@@ -42,9 +36,9 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     FormsModule,
     AppRoutingModule,
     Ng2CarouselamosModule,
-    DurationPickerModule,
+    DurationPickerModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
