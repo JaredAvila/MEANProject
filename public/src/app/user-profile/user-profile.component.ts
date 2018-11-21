f4bb8758298642169f9b7b8d2958189675df2092
Jared Avila <jared.c.avila@gmail.com>
2018-11-21 12:02:05 -0800

    updated backend userProfile, updated front end userProfile

--- public/src/app/user-profile/user-profile.component.ts
+++ public/src/app/user-profile/user-profile.component.ts
@@ -1,82 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service'; 
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../http.service";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.component.html",
+  styleUrls: ["./user-profile.component.css"]
 })
 export class UserProfileComponent implements OnInit {
-  id : string
-  user : object
-  profileToPass : object
-  walletToPass : object
-  auctionsWatchedToPass : object
-  auctionsCreatedToPass : object
-
+  id: string;
+  user: object;
+  profileToPass: object;
+  auctionsWatchedToPass: object;
+  auctionsCreatedToPass: object;
 
   constructor(
     private _httpService: HttpService,
     private _route: ActivatedRoute,
     private _router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.getId()
+    this.getId();
     this.user = {
-      auctions_created  : [],
-      auctions_watched : []
-    }
-    this.showProfile()
+      auctions_created: [],
+      auctions_watched: []
+    };
+    this.showProfile();
   }
   getId() {
-    this._route.params.subscribe((params) => {
-      this.id = params['id'];
+    this._route.params.subscribe(params => {
+      this.id = params["id"];
       this.getUser(this.id);
-    })
+    });
   }
 
   getUser(id) {
     let obs = this._httpService.getUserById(id);
     obs.subscribe(res => {
-      this.user = res['data'][0];
-      this.profileToPass = this.user
-    })
+      this.user = res["data"][0];
+      this.profileToPass = this.user;
+    });
   }
 
   showProfile() {
-    this.profileToPass = this.user
-    this.walletToPass = null
-    this.auctionsWatchedToPass = null
-    this.auctionsCreatedToPass = null
-    // this.getUser(this.id)
+    this.profileToPass = this.user;
+    this.walletToPass = null;
+    this.auctionsWatchedToPass = null;
+    this.auctionsCreatedToPass = null;
   }
 
-  showWallet() {
-    this.profileToPass = null
-    this.walletToPass = this.user
-    this.auctionsWatchedToPass = null
-    this.auctionsCreatedToPass = null
+  updateUserProfile($event) {
+    this.getUser(this.id);
   }
-
-
-  showAuctionsWatched() {
-    this.profileToPass = null
-    this.walletToPass = null
-    this.auctionsWatchedToPass = this.user
-    this.auctionsCreatedToPass = null
-  }
-
-  showAuctionsCreated() {
-    this.profileToPass = null
-    this.walletToPass = null
-    this.auctionsWatchedToPass = null
-    this.auctionsCreatedToPass = this.user
-  }
-
-  updateUserProfile($event){
-    this.getUser(this.id)
-  }
-
 }
